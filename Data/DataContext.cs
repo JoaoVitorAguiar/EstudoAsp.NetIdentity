55c0eece699454dbdb3684e8c3b2de79bf511098
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2023-11-24 08:57:32 -0300

    fix: add comments

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,17 +1,23 @@
 ﻿using EstudoAsp.NetIdentity.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EstudoAsp.NetIdentity.Data;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     { }
+
+    protected DataContext()
+    {
+    }
 
     public DbSet<Student> Students { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Executa quando o cria o banco
         base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Student>().HasData(
             new Student
